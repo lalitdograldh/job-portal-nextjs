311d15498caf396a7669e68ed8b2b64afae10245
lalitdograldh <lalitdogra04@gmail.com>
2025-10-07 23:03:36 +0530

    use select wirth label

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import React, { useState } from 'react'
 import { Input } from "@/components/ui/input"
-
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 interface RegistrationFormData {
     name : string;
     userName :string;
@@ -22,7 +29,23 @@ const Registration: React.FC= () => {
   return (
     <div>
         <form>
-         <div>{<Input type="email" placeholder="Email" />}</div>
+            <div className="grid w-full max-w-sm items-center gap-3">
+                <Label htmlFor="username">userName</Label>
+                <Input id="username" type="text" />
+            </div>
+            <div className="flex flex-col space-y-2">
+                <Label htmlFor="theme">Theme</Label>
+                <Select>
+                    <SelectTrigger id="theme" className="w-[180px]">
+                    <SelectValue placeholder="Select theme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                    <SelectItem value="light">Light</SelectItem>
+                    <SelectItem value="dark">Dark</SelectItem>
+                    <SelectItem value="system">System</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
         </form>
     </div>
   )
