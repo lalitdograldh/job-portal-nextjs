89137fa9c1815b5ee71b5813706e3f1c8773c1d6
lalitdograldh <lalitdogra04@gmail.com>
2025-10-31 20:38:48 +0530

    use controll;er in select in registration form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -23,7 +23,7 @@ import {
 import { Eye, EyeOff, Lock, Mail, User, UserCheck } from "lucide-react";
 import { registerUserAction } from '../../features/auth/server/auth.actions';
 import { toast } from 'sonner';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { RegisterUserWithConfirmData, registerUserWithConfirmSchema } from '@/features/auth/auth.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 
@@ -31,7 +31,7 @@ const Registration: React.FC= () => {
    const {
     register,
     handleSubmit,
-    watch,
+    control,
     formState: { errors },
   } = useForm({
     resolver: zodResolver(registerUserWithConfirmSchema),
@@ -120,15 +120,25 @@ const onSubmit = async (data:RegisterUserWithConfirmData) => {
                     </div>
                     <div className="space-y-2 w-full">
                         <Label htmlFor="role">I am a *</Label>  
-                        <Select {...register("role")} >
-                           <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Select your role" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="applicant">Job Applicant</SelectItem>
-                                <SelectItem value="employer">Employer</SelectItem>
-                            </SelectContent>
-                        </Select>
+                        <Controller name='role' control={control} render={({ field }) => (
+                            <Select value={field.value} onValueChange={field.onChange}>
+                                <SelectTrigger className={`w-full pl-10 pr-10 ${
+                                        errors.role ? "border-destructive" : ""
+                                    }`}>
+                                    <SelectValue placeholder="Select your role" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="applicant">Job Applicant</SelectItem>
+                                    <SelectItem value="employer">Employer</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        )}>
+                        </Controller>
+                        {errors.role && (
+                            <p className="text-sm text-destructive">
+                                {errors.role.message}
+                            </p>
+                        )}
                     </div>
                     <div className="space-y-2">
                         <Label htmlFor="password">Password *</Label>
