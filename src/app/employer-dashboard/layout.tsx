60c63d41dd6de29ebca1d09a2cfe3653050f2a90
lalitdograldh <lalitdogra04@gmail.com>
2025-11-13 19:35:09 +0530

    employer dasboard with show user name which login at that time and also working with sidebar

--- src/app/employer-dashboard/layout.tsx
+++ src/app/employer-dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { getCurrentUser } from "@/features/auth/server/auth.queries";
+import EmployerSidebar from "@/features/employers/components/employer-sidebar";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
@@ -7,10 +8,17 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }>) {
     const user =await getCurrentUser();
-    console.log("user data :",user);
+    //console.log("user data :",user);
     
     if (!user) return redirect('/login');
 
     if (user.role !== "employer") return redirect('/dashboard');
-  return <>{children}</> ;
+  return (
+    
+    <div className="flex min-h-screen bg-background">
+      <EmployerSidebar/>
+      <main className="container mx-auto mt-5 ml-70 mr-5">{children}</main>      
+    </div> 
+
+  );
 }
